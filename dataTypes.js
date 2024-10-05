c5b608d771a6a3cdc12f75ef34b9fdfed5195067
Gaurav Singh Nagarkoti <142169880+NagarkotiGaurav@users.noreply.github.com>
2024-10-05 18:01:59 +0000

    started again

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+/* String 
+    number     NaN is the only value in js which is not equal to itself
+    boolean     [rest all are typeOf their name  ie. number is number , symbol is symbol and so on ]
+    bigInt      we cant use >>> opertor with bigInt 
+    symbol
+    Undefined 
+    null       **(it is a typeOf object)
+    object
+
+    objects: 
+    object 
+    array 
+    set 
+    map 
+    promises etc.
+
+
+*/
+
+let num = 1000;
